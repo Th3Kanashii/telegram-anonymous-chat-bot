6b1f7ae1702a7c28352b9cea638196ab2ced5ad3
Th3Kanashii <Th3Kanashii@gmail.com>
2024-03-21 19:49:03 +0200

    Add logging

--- bot/__main__.py
+++ bot/__main__.py
@@ -4,9 +4,11 @@ from aiogram import Bot, Dispatcher
 
 from .config import Config
 from .core import create_bot, create_dispatcher, run_polling
+from .utils import setup_logging
 
 
 async def main() -> None:
+    setup_logging()
     config: Config = Config()
     dispatcher: Dispatcher = create_dispatcher(config=config)
     bot: Bot = create_bot(config=config)
---
+++ bot/utils/__init__.py
@@ -0,0 +1,3 @@
+from .logging import setup_logging
+
+__all__ = ["setup_logging",]
